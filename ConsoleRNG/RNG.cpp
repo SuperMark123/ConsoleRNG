1374585a2110478cf4b60647d32549346714d110
Zhaobin Li <zbli@bu.edu>
2024-11-12 21:17:03 -0500

    Making 0 and 1 equally possible as other quantiles

--- ConsoleRNG/RNG.cpp
+++ ConsoleRNG/RNG.cpp
@@ -49,13 +49,25 @@ std::size_t RNG::generateRandInt() const
 std::shared_ptr<std::vector<double>> RNG::generate(std::size_t n) const
 {
 	std::shared_ptr<std::vector<double>> randNums = std::make_shared<std::vector<double>>(n);
+	std::size_t segmentLen = std::floor(SIZE_MAX / (1/mAccuracy + 1));
 	for (std::size_t i = 0; i < n; ++i)
 	{
-		// Use the generated random unsigned int to compute the random probability
-		double prob = 1.0 * generateRandInt() / SIZE_MAX;
-		prob = std::round(prob / mAccuracy) * mAccuracy;
+		/*
+		* Use the generated random unsigned int to compute the random probability.
+		* There will be 10^AccuracyLevel + 1 probabilities to be chosen. 
+		* To make all probabilities equally possible, all possible random integers are equally divided into 10^accuracy + 1 segments.
+		* Assign (i-1)*mAccuracy to prob if the random int lies between [(i-1)*segmentLen, i*segmentLen).
+		* If the random int is greater than (10^AccuracyLevel + 1) * segmentLen, regenerate the random int once again.
+		* Given that SIZE_MAX mod (1/mAccuracy + 1) <= 1/mAccuracy and generateRandInt() mod segmentLen < segmentLen,
+		* the probability of regenerating the random int is less than 2 * segmentLen / SIZE_MAX <= 2 * mAccuracy (accuracyLevel <= 5)
+		*/
+		double prob = mAccuracy * std::floor(1.0 * generateRandInt() / segmentLen);
+		while (prob > 1)
+		{
+			prob = mAccuracy * std::floor(generateRandInt() / segmentLen);
+		}
 		// Debug print probability
-		// std::cout << prob << ", ";
+		//std::cout << prob << ", ";
 		double res = mDist->invCDF(prob, *mSolver);
 		(*randNums)[i] = std::round(res / mAccuracy) * mAccuracy;
 	}
